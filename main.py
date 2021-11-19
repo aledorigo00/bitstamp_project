0fe5698685c3d9c6c0e615d5bebbc9fbb31ea57b
Alessandro Dorigo <alessandrodorigo@MacBook-Pro.local>
2021-11-19 17:44:08 +0100

    Timeseries

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import time
 from bitstamp import get_price
 
-eur=get_price("btceur")
-print("Last bitcoin price in EUR is: {}".format(eur))
-dollar=get_price("btcusd")
-print("Last bitcoin price in $ is: {}".format(dollar))
+timeseries_eur=[]
+timeseries_dollar=[]
 
 
+for n in range(0,10):
+	eur=get_price("btceur")
+	timeseries_eur.append(eur)
+	print("Last bitcoin price in EUR is: {}".format(eur))
+	dollar=get_price("btcusd")
+	timeseries_dollar.append(dollar)
+	print("Last bitcoin price in $ is: {}".format(dollar))
+	time.sleep(1)
+
+
+print(timeseries_dollar)
