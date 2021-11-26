e779c049d33238fe6cc4f893dcb374675cdde48c
Marco Signora <marcosignora@MacBook-Air-di-Marco-2.local>
2021-11-26 18:02:28 +0100

    added explanations

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 
 
-import time
-from bitstamp import get_price
-import argparse
+import time #imported time module
+from bitstamp import get_price #import modules to fetch information from a public API 
+import argparse #imported argparse module
 
+#Here we add two arguments that the users can input, namely crypto and fiat
 parser = argparse.ArgumentParser()
 parser.add_argument("crypto", help="Type crypto ticker (btc, eth)")
 parser.add_argument("fiat", help="Type fiat ticker (eur, usd)")
 args = parser.parse_args()
 
+#The case in which the fiat selection is eur
 if args.fiat == 'eur':
+
+	#if the typed coin is btc, then return the price of btc in euro.
+	
 	if args.crypto == "btc":
 		to_print = get_price("btceur")
+
+	#Instead if the typed coin is eth, then return the price of eth in euro.
 	elif args.crypto == "eth":
 		to_print = get_price("etheur")
+
+#The case in which the fiat selection is usd		
 else:
+
+	#if the typed coin is btc, then return the price of btc in dollars.
 	if args.crypto == "btc":
 		to_print = get_price("btcusd")
+
+	#Instead if the typed coin is eth, then return the price of eth in euro.
 	elif args.crypto == "eth":
 		to_print = get_price("ethusd")
 
