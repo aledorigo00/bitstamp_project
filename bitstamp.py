3f9ecf6e60d8239d2746002a2775fa851766c001
Alessandro Dorigo <alessandrodorigo@MacBook-Pro.fritz.box>
2021-11-26 22:18:27 +0100

    completed multiple days

--- bitstamp.py
+++ bitstamp.py
@@ -1,9 +1,35 @@
 import requests
 import json
+from datetime import datetime
 
 
 def get_price(currency):
+    '''
+    This function returns the price of a crypto/fiar currency pair at the moment.
+    It takes as input the currency pair.
+    '''
     bitstamp_URL = 'https://www.bitstamp.net/api/v2/ticker/%s/'
     r = requests.get(bitstamp_URL % currency)
     price = float(json.loads(r.text)['last'])
     return price
+
+
+def get_time_series(start, currency):
+    '''
+    This functions returns the time series of a crypto/fiat currency pair
+    prices, taking as input two parameters: 
+        - start: the timestamp that indicates the beginning of the searching period
+        - currency: the currency pair to be retreived (e.g. btceur, ethusd)
+    '''
+    time_series_URL = 'https://www.bitstamp.net/api/v2/ohlc/'+currency+'/'
+    r = requests.get(time_series_URL, params={'start':start, 'step':1800, 'limit':100})
+    total = json.loads(r.text)
+    data=total['data']
+    ohlc=data['ohlc']
+    
+    dates = []
+    values = []
+    for point in ohlc:
+        dates.append(datetime.fromtimestamp(int(point['timestamp'])))
+        values.append(float(point['close']))
+    return dates, values
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from bitstamp import get_price #import modules to fetch information from a public API 
 import argparse #imported argparse module
+import time #imported time module
+from datetime import datetime, timezone
 
-#Here we add two arguments that the users can input, namely crypto and fiat
+#import modules to fetch information from a public API 
+from bitstamp import get_price 
+from bitstamp import get_time_series
+
+
+#Here we add the arguments that the users can input, namely crypto currency, 
+#fiat currency and the optional argument date
 parser = argparse.ArgumentParser()
 parser.add_argument("crypto", help="Type crypto ticker (btc, eth)")
 parser.add_argument("fiat", help="Type fiat ticker (eur, usd)")
@@ -9,28 +16,45 @@ parser.add_argument('-d',"--date", help="Type date dd/mm/yyyy")
 args = parser.parse_args()
 
 
-#The case in which the fiat selection is eur
-if args.fiat == 'eur':
+#check if the optional argument date is flagged or not
+if args.date is not None:
+	#split the input string into year, month and day
+	day=int(args.date.split('/')[0])
+	month=int(args.date.split('/')[1])
+	year=int(args.date.split('/')[2])
 
-	#if the typed coin is btc, then return the price of btc in euro.
-	
-	if args.crypto == "btc":
-		to_print = get_price("btceur")
+	#create a datetime from the data entered by the user and convert to 
+	#the Unix timestamp format required by the API
+	dt = datetime( year, month, day, 00, 00, 00, tzinfo=timezone.utc )
+	timestamp = int( dt.timestamp() )
 
-	#Instead if the typed coin is eth, then return the price of eth in euro.
-	elif args.crypto == "eth":
-		to_print = get_price("etheur")
+	#execute the function
+	dates,values = get_time_series(timestamp,'btceur')
+	print(dates)
+	print(values)
 
-
-#The case in which the fiat selection is usd		
 else:
+	#The case in which the fiat selection is eur
+	if args.fiat == 'eur':
 
-	#if the typed coin is btc, then return the price of btc in dollars.
-	if args.crypto == "btc":
-		to_print = get_price("btcusd")
+		#if the typed coin is btc, then return the price of btc in euro.
+		if args.crypto == "btc":
+			to_print = get_price("btceur")
 
-	#Instead if the typed coin is eth, then return the price of eth in euro.
-	elif args.crypto == "eth":
-		to_print = get_price("ethusd")
+		#Instead if the typed coin is eth, then return the price of eth in euro.
+		elif args.crypto == "eth":
+			to_print = get_price("etheur")
 
-print(to_print)
+
+	#The case in which the fiat selection is usd		
+	else:
+
+		#if the typed coin is btc, then return the price of btc in dollars.
+		if args.crypto == "btc":
+			to_print = get_price("btcusd")
+
+		#Instead if the typed coin is eth, then return the price of eth in euro.
+		elif args.crypto == "eth":
+			to_print = get_price("ethusd")
+
+	print(to_print) #print the result
